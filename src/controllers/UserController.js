9c89b824c69a587a39eae760e122587506e5d44b
Gabriel Gonçalves <gabrielgrv2015@outlook.com>
2021-10-01 16:37:51 -0300

    Initializing user controller.

---
+++ src/controllers/UserController.js
@@ -0,0 +1,18 @@
+const User = require('../models/User')
+
+module.exports = {
+    async create(req, res) {
+        const { username, cpf } = req.body
+
+        const alreadyExists = await User.findOne({ where: { [Op.or]: [{ username }, { cpf }] } })
+
+        if (alreadyExists) {
+            console.log('Error: user already exists');
+        }
+
+        await User.create(req.body)
+        return res.json({ msg: 'New user created!' })
+    },
+
+
+}
