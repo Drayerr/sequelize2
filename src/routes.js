c4338e62ff9782e5cc9847d1c372a7e0cc967434
Drayerr <gabrielgrv2015@outlook.com>
2021-10-02 20:55:42 -0300

    Bug fix.

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,13 @@
 const express = require('express')
+const UserController = require('./controllers/UserController')
 
 const routes = express.Router()
 
 routes.get('/', (req, res) => {
     return res.send('Home')
 })
+
+routes.post('/user', UserController.create)
 
 routes.get('/teste', (req, res) => {
     return res.send('teste')
